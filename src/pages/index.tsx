c725569888af0a8c501cfac2d1f35767fa9d4e35
takacube <takataka1409@docomo.ne.jp>
2022-11-14 12:54:31 +0900

    fix: rm extra import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import MultiActionAreaCard from '../components/blogCard'
 import BlogCardList from '../components/blogCardList'
 import Header from '../components/header'
 import styles from '../styles/Home.module.css'
